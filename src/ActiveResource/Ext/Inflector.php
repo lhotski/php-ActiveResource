6f8b92023f777ee7272d9e7547c3fad3305b5f4f
Dmitry Prokudin <lhotski@gmail.com>
2011-12-24 05:03:58 +0400

    Fixed some inflector rules, pure tests added

--- src/ActiveResource/Ext/Inflector.php
+++ src/ActiveResource/Ext/Inflector.php
@@ -84,34 +84,45 @@ class Inflector
     public static function pluralize($word)
     {
       $plurals = array(
-        '/(quiz)$/i'                => '1zes',
-        '/^(ox)$/i'                 => '1en',
-        '/([m|l])ouse$/i'           => '1ice',
-        '/(matr|vert|ind)ix|ex$/i'  => '1ices',
-        '/(x|ch|ss|sh)$/i'          => '1es',
-        '/([^aeiouy]|qu)ies$/i'     => '1y',
-        '/([^aeiouy]|qu)y$/i'       => '1ies',
-        '/(hive)$/i'                => '1s',
-        '/(?:([^f])fe|([lr])f)$/i'  => '12ves',
-        '/sis$/i'                   => 'ses',
-        '/([ti])um$/i'              => '1a',
-        '/(buffal|tomat)o$/i'       => '1oes',
-        '/(bu)s$/i'                 => '1ses',
-        '/(alias|status)/i'         => '1es',
-        '/(octop|vir)us$/i'         => '1i',
-        '/(ax|test)is$/i'           => '1es',
+        '/(quiz)$/i'                => '\1zes',
+        '/^(ox)$/i'                 => '\1en',
+        '/([m|l])ouse$/i'           => '\1ice',
+        '/(matr|vert|ind)ix|ex$/i'  => '\1ices',
+        '/(x|ch|ss|sh)$/i'          => '\1es',
+        '/([^aeiouy]|qu)ies$/i'     => '\1y',
+        '/([^aeiouy]|qu)y$/i'       => '\1ies',
+        '/(hive)$/i'                => '\1s',
+        '/(?:([^f])fe|([lr])f)$/i'  => '\1\2ves',
+        '/sis$/i'                   => '\1ses',
+        '/([ti])um$/i'              => '\1a',
+        '/(buffal|tomat)o$/i'       => '\1oes',
+        '/(bu)s$/i'                 => '\1ses',
+        '/(alias|status)/i'         => '\1es',
+        '/(octop|vir)us$/i'         => '\1i',
+        '/(ax|test)is$/i'           => '\1es',
         '/s$/i'                     => 's',
         '/$/'                       => 's'
       );
       $uncountables = array(
-        'equipment', 'information', 'rice', 'money', 'species', 'series', 'fish', 'sheep'
+        'equipment', 'information', 'rice', 'species', 'series', 'fish', 'sheep'
       );
       $irregulars = array(
-        'person'  => 'people',
-        'man'     => 'men',
-        'child'   => 'children',
-        'sex'     => 'sexes',
-        'move'    => 'moves'
+        'person'    => 'people',
+        'man'       => 'men',
+        'child'     => 'children',
+        'sex'       => 'sexes',
+        'move'      => 'moves',
+        'atlas'     => 'atlases',
+        'corpus'    => 'corpuses',
+        'genus'     => 'genera',
+        'graffito'  => 'graffiti',
+        'loaf'      => 'loaves',
+        'money'     => 'monies',
+        'mythos'    => 'mythoi',
+        'numen'     => 'numina',
+        'penis'     => 'penises',
+        'soliloquy' => 'soliloquies',
+        'turf'      => 'turfs'
       );
       $lowerCasedWord = strtolower($word);
       foreach ($uncountables as $uncountable) {
@@ -176,11 +187,24 @@ class Inflector
         'equipment', 'information', 'rice', 'money', 'species', 'series', 'fish', 'sheep'
       );
       $irregulars = array(
-        'person'  => 'people',
-        'man'     => 'men',
-        'child'   => 'children',
-        'sex'     => 'sexes',
-        'move'    => 'moves'
+        'person'    => 'people',
+        'man'       => 'men',
+        'child'     => 'children',
+        'sex'       => 'sexes',
+        'move'      => 'moves',
+        'atlas'     => 'atlases',
+        'corpus'    => 'corpuses',
+        'genus'     => 'genera',
+        'genie'     => 'genies',
+        'graffito'  => 'graffiti',
+        'loaf'      => 'loaves',
+        'money'     => 'monies',
+        'mythos'    => 'mythoi',
+        'niche'     => 'niches',
+        'numen'     => 'numina',
+        'penis'     => 'penises',
+        'soliloquy' => 'soliloquies',
+        'turf'      => 'turfs'
       );
       $lowerCasedWord = strtolower($word);
       foreach ($uncountables as $uncountable) {
---
+++ tests/ActiveResource/Ext/InflectorTest.php
@@ -0,0 +1,58 @@
+<?php
+
+use ActiveResource\Ext\Inflector;
+
+class InflectorTest extends PHPUnit_Framework_TestCase
+{
+
+  /**
+   * @dataProvider pluralizeDataProvider
+   */
+  public function testPluralize($source, $result)
+  {
+    $this->assertEquals($result, Inflector::pluralize($source));
+  }
+
+  /**
+   * @dataProvider pluralizeDataProvider
+   */
+  public function testSingularize($result, $source)
+  {
+    $this->assertEquals($result, Inflector::singularize($source));
+  }
+
+  public function pluralizeDataProvider()
+  {
+    return array(
+      array('status', 'statuses'),
+      array('quiz', 'quizzes'),
+      array('buffalo', 'buffaloes'),
+      array('man', 'men'),
+      array('axis', 'axes'),
+      array('testis', 'testes'),
+      array('crisis', 'crises'),
+      array('dwarf', 'dwarves'),
+      array('mouse', 'mice'),
+      array('ox', 'oxen'),
+      array('atlas', 'atlases'),
+      array('beef', 'beefs'),
+      array('corpus', 'corpuses'),
+      array('cow', 'cows'),
+      array('ganglion', 'ganglions'),
+      array('genie', 'genies'),
+      array('genus', 'genera'),
+      array('graffito', 'graffiti'),
+      array('hoof', 'hoofs'),
+      array('loaf', 'loaves'),
+      array('money', 'monies'),
+      array('mongoose', 'mongooses'),
+      array('move', 'moves'),
+      array('niche', 'niches'),
+      array('numen', 'numina'),
+      array('occiput', 'occiputs'),
+      array('penis', 'penises'),
+      array('turf', 'turfs'),
+    );
+  }
+
+}
